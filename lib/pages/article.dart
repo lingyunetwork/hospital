9eb5f43ff667ae7370766017cf5ee416dcb0d2a8
carl <crlclr@gmail.com>
2019-09-14 03:07:44 +0800

    图片太大的真机上运行不动，尝试切成多张图

--- lib/pages/article.dart
+++ lib/pages/article.dart
@@ -7,34 +7,27 @@ class ArticlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*final cancelBtn = Positioned(
-      top: 50.0,
-      left: 20.0,
-      child: Container(
-        height: 35.0,
-        width: 35.0,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.grey.withOpacity(0.5),
-        ),
-        child: IconButton(
-          icon: Icon(Icons.close, color: Colors.white),
-          onPressed: () => Navigator.pop(context),
-          iconSize: 20.0,
-        ),
-      ),
-    );*/
+    var url = vo.url;
 
-    Widget ui = Image.asset(
-      vo.url,
-      fit: BoxFit.fitWidth,
-    );
+    var list = url.split(",");
+
+    Widget ui = ListView.builder(
+        shrinkWrap:true,
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          var url = list[index];
+          return Image.asset(
+            url,
+            fit: BoxFit.fitWidth,
+          );
+        });
 
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
           title: Text(vo.name),
         ),
-        body: SingleChildScrollView(child: ui));
+        body: ui,
+    );
   }
 }
